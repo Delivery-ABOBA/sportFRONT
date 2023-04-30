759e1f276ea1286a74d01747211ba57781955ab9
Delivery-ABOBA <87445627+Delivery-ABOBA@users.noreply.github.com>
2023-04-30 23:02:49 +0300

    Update test.js

--- source/test.js
+++ source/test.js
@@ -90,6 +90,10 @@ function Matches(item){
   print(item);
   var parent=document.getElementById("container");
   parent.innerHTML='<div class="mb-8"><h2 class="fw-bold m-0">'+DataMatches.Stages[item].Snm+'</h2></div><div class="card-list" id = "liga"></div>';
+  var parent=document.getElementById("liga");
+  for(i=0; i<=DataMatches.Stages[item].Events.length; i++){
+      parent.innerHTML+='<a href="#" class="card border-0 text-reset"><div class="card-body"><div class="row gx-5"><div class="col"><div class="d-flex align-items-center mb-3"><h5 class="me-auto mb-0">'+DataMatches.Stages[item].Events[i].T1[0].Nm+'</h5></div></div></div></div></a>';
+  }
 }
 
 function GoBack(){
