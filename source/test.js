be1a9fbda8e1f8d404791a9d178f6d68bddaf46b
Delivery-ABOBA <87445627+Delivery-ABOBA@users.noreply.github.com>
2023-05-02 20:54:18 +0300

    Update test.js

--- source/test.js
+++ source/test.js
@@ -58,7 +58,7 @@ function CustomXHR(method, path, authorization=true){
 }
 
 function videos(){
-    var xhr = CustomXHR("GET", "/sport/news");
+    var xhr = CustomXHR("GET", "/sport/live");
     xhr.onreadystatechange = function(){
     if (xhr.readyState === 4 && xhr.status === 200){
     var items = JSON.parse(xhr.responseText);
